cb380e7af71bd3af2aeae4cce618740ffe83e9ef
Hazel Preza <hazel.d.preza@gmail.com>
2023-03-03 09:52:14 -0600

    very confused, trying more stuff

--- src/components/App.js
+++ src/components/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>hello i'm hazel</p>
+        <p>hmmmm</p>
       </header>
     </div>
   );
